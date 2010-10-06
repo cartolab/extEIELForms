26bbd6d1b036aa78df8a3e11982029ec22256a05
Javier Estévez <jestevez@cartolab.es>
2010-10-06 13:51:59 +0200

    Throw form after every geometry insertion

--- src/es/udc/cartolab/gvsig/eielforms/FormExtension.java
+++ src/es/udc/cartolab/gvsig/eielforms/FormExtension.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (c) 2010. Cartolab (Universidade da Coruña)
- * 
+ *
  * This file is part of extEIELForms
- * 
+ *
  * extEIELForms is based on the forms application of GisEIEL <http://giseiel.forge.osor.eu/>
  * devoloped by Laboratorio de Bases de Datos (Universidade da Coruña)
- * 
+ *
  * extEIELForms is free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or any later version.
- * 
+ *
  * extEIELForms is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with extEIELForms.
  * If not, see <http://www.gnu.org/licenses/>.
  */
@@ -64,7 +64,7 @@ public class FormExtension extends Extension {
 		View v = (View) PluginServices.getMDIManager().getActiveWindow();
 		FLayers layers = v.getMapControl().getMapContext().getLayers();
 		if (layers.getActives().length > 0) {
-			return layers.getActives()[0] instanceof FLyrVect;
+			return layers.getActives()[0] instanceof FLyrVect && !layers.getActives()[0].isEditing();
 		} else {
 			return false;
 		}
