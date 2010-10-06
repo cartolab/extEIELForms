17ca7b257bc0cb54f92d958f588f5b79e1c6728a
Javier Estévez <jestevez@cartolab.es>
2010-10-06 17:31:12 +0200

    Load form default value on field if there's null on database

--- src/es/udc/cartolab/gvsig/eielforms/field/TextFieldInterface.java
+++ src/es/udc/cartolab/gvsig/eielforms/field/TextFieldInterface.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (c) 2010. Cartolab (Universidade da Coruña)
- * 
+ *
  * This file is part of extEIELForms
- * 
+ *
  * extEIELForms is based on the forms application of GisEIEL <http://giseiel.forge.osor.eu/>
  * devoloped by Laboratorio de Bases de Datos (Universidade da Coruña)
- * 
+ *
  * extEIELForms is free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or any later version.
- * 
+ *
  * extEIELForms is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with extEIELForms.
  * If not, see <http://www.gnu.org/licenses/>.
  */
@@ -69,7 +69,8 @@ public class TextFieldInterface extends FieldInterface
 	@Override
 	public void loadValue()
 	{
-		this.textField.setText(this.fieldController.getValue());
+		String value = this.fieldController.getValue();
+		this.textField.setText(value);
 		validate();
 	}
 
